7190a179630cfc7889668c78c77ea31264fd66c9
han <yessetkhan@gmail.com>
2025-10-15 19:27:23 -0400

    fix: minor

---
+++ entity-module/customreportbuilder/ui/src/assets/queries/dims/brands.sql
@@ -0,0 +1,8 @@
+-- brands.sql
+SELECT
+  CAST(b.brand_id AS STRING) AS id,
+  b.brand_name               AS name
+FROM analytics.mm.dim_brand AS b
+WHERE b.brand_id IS NOT NULL AND b.brand_name IS NOT NULL
+GROUP BY b.brand_id, b.brand_name
+ORDER BY name;
